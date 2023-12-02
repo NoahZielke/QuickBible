b4c9133770cc8071298ef373e675be90ef8517e7
Noah Zielke <noahzielke19@gmail.com>
2023-12-02 17:10:29 -0500

    Change subtitle

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <div className="container-fluid">
             <div className="pb-1">
                 <h3 className="main-title px-2 pt-2 mb-0">Quick Bible</h3>
-                <p className="subtitle mt-0 ps-4 pe-2"><i>An ad-free, lighting-fast Bible search</i></p>
+                <p className="subtitle mt-0 ps-4 pe-2"><i>Ad-free, lighting-fast Bible search</i></p>
             </div>
             <Display versionList={versionList} />
         </div>
