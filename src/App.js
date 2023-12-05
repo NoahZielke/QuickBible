f5723e333bd1e25ed08428657ad0579bdfb41fee
Noah <noah.zielke@adtran.com>
2023-12-05 14:44:58 -0600

    Another tweak to margins on homepage

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
                 <h3 className="main-title px-3 pt-3 mb-0">Quick Bible</h3>
                 <p className="subtitle mt-0 ps-5 pe-2"><i>Ad-free, lighting-fast Bible search</i></p>
             </div>
-            <div className="container-fluid pt-5">
+            <div className="py-2"></div>
+            <div className="container-fluid pt-4">
                 <Display versionList={versionList} />
             </div>
         </div>
