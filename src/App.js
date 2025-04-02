e2d9f04595f2d925a0d90cd19ea3be4c6d01b6ae
Noah <noahzielke19@yahoo.com>
2025-04-02 15:53:17 -0500

    Remove KJV25

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import "./app.css"
 import Display from "./components/Display";
-import KJV25FullText from "./resources/KJV25FullText";
 import KJVFullText from "./resources/KJVFullText";
 import WEBFullText from "./resources/WEBFullText";
 
 
 function App() {
-    const KJV25 = KJV25FullText();
     const KJV = KJVFullText();
     const WEB = WEBFullText();
-    const versionList = [KJV25, KJV, WEB];
+    const versionList = [KJV, WEB];
 
     return (
         <div>
